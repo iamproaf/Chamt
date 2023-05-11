c2200991db25eb03149d0d89bec8626cdcabdc24
team-katil <115205040+team-katil@users.noreply.github.com>
2023-05-11 17:00:19 +0530

    Create stats.py

---
+++ Adhya/modules/stats.py
@@ -0,0 +1,18 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Adhya import OWNER, dev
+from Adhya.database.chats import get_served_chats
+from Adhya.database.users import get_served_users
+
+
+@adhya.on_message(filters.command("stats") & filters.user(OWNER))
+async def stats(cli: adhya, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
